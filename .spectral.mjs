fae9edbc1244035e8811fa4e28b5402e86e0309a
georgedrak <george@drakakis.gr>
2023-02-25 21:42:00 +0200

    Modified 18 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjEzOTQ2NQ/spectral.js?branch_slug=main&token=cf69cbee-fab2-427a-8c9a-f60686b76d35";
+export default { extends: ruleset };
